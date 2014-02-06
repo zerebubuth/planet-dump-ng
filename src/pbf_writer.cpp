b56b920bf7af12392d6e345aa3ee4ab0c174cd57
Matt Amos <zerebubuth@gmail.com>
2014-02-06 00:48:15 +0000

    Bring recheck interval down for relations so they don't overflow.

--- src/pbf_writer.cpp
+++ src/pbf_writer.cpp
@@ -98,7 +98,7 @@ struct pbf_writer::pimpl {
     m_recheck_elements[element_CHANGESET] = 1;
     m_recheck_elements[element_NODE] = 16000;
     m_recheck_elements[element_WAY] = 8000;
-    m_recheck_elements[element_RELATION] = 4000;
+    m_recheck_elements[element_RELATION] = 2000;
 
     write_header_block(now);
   }
